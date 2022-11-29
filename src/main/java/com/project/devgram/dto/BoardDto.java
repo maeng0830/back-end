7ed7e123f0f60bddd39e9a98458d30346ae799e7
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-11-30 03:03:47 +0900

    Feat : Board 등록 API 추가

---
+++ src/main/java/com/project/devgram/dto/BoardDto.java
@@ -0,0 +1,28 @@
+package com.project.devgram.dto;
+
+import com.project.devgram.domain.Board;
+import com.project.devgram.type.status.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDto {
+	private String title;
+	private String content;
+	private Status status;
+	private Integer likeCount;
+
+	public static BoardDto fromEntity(Board board){
+		return BoardDto.builder()
+			.title(board.getTitle())
+			.content(board.getContent())
+			.status(board.getStatus())
+			.likeCount(board.getLikeCount())
+			.build();
+	}
+}
