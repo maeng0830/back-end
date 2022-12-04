29b12a0bee0adb04983e5efb92492547f50384f9
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-12-04 19:16:09 +0900

    Feat : Board 관련 update, delete 메소드 추가

--- src/main/java/com/project/devgram/controller/BoardController.java
+++ src/main/java/com/project/devgram/controller/BoardController.java
@@ -2,13 +2,17 @@ package com.project.devgram.controller;
 
 import com.project.devgram.dto.RegisterBoard;
 import com.project.devgram.dto.SearchBoard;
+import com.project.devgram.dto.UpdateBoard;
 import com.project.devgram.service.BoardService;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,4 +36,15 @@ public class BoardController {
 	public List<SearchBoard.Response> searchBoards(@ModelAttribute SearchBoard.Request request){
 		return SearchBoard.Response.listOf(boardService.searchBoards(request));
 	}
+
+	@PutMapping
+	public UpdateBoard.Response updateBoard(@RequestBody UpdateBoard.Request request){
+		return UpdateBoard.Response.of(boardService.updateBoard(request));
+	}
+
+	@DeleteMapping("/{boardSeq}")
+	public void deleteBoard(@PathVariable Long boardSeq){
+		boardService.deleteBoard(boardSeq);
+	}
+
 }
--- src/main/java/com/project/devgram/entity/Board.java
+++ src/main/java/com/project/devgram/entity/Board.java
@@ -10,18 +10,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Board extends BaseTimeEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "board_seq", nullable = false)
 	private Long boardSeq;
 
-	@Column(length = 100, name= "title")
+	@Column(length = 100, name = "title")
 	private String title;
 
 	@Column(columnDefinition = "TEXT")
@@ -30,14 +32,40 @@ public class Board extends BaseTimeEntity {
 	@Column(length = 10)
 	private Status status;
 
-	@Column(name="like_count")
+	@Column(name = "like_count")
 	private Integer likeCount;
 
 	@Builder
-	public Board(String title, String content){
+	public Board(String title, String content) {
 		this.title = title;
 		this.content = content;
 		this.status = Status.NORMAL;
 		this.likeCount = 0;
 	}
+
+	public void deleteBoard() {
+		this.status = Status.DELETED;
+	}
+
+	public void updateTitle(String title) {
+		checkIsDeleted();
+		if (StringUtils.hasText(title)) {
+			this.title = title;
+		}
+	}
+
+	public void updateContent(String content) {
+		checkIsDeleted();
+		if (StringUtils.hasText(content)) {
+			this.content = content;
+		}
+	}
+
+	private void checkIsDeleted() {
+		//TODO : MERGE 이후 적절한 Exception 만들어 해당 내용 변경
+		if (this.status == Status.DELETED) {
+			throw new RuntimeException("이미 삭제된 보드 입니다. ");
+		}
+	}
+
 }
